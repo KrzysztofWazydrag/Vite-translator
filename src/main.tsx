d3311fb5ea787d7b7e37e36b05a9ccfd0e6908d5
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-11-15 17:32:47 +0000

    Add vite config and refactor code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.tsx'
+import { App } from './app'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 )
