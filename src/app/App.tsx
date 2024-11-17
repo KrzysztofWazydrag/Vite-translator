f19f015b139db8d010ee77f5e88a799b3142ce15
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2024-11-17 21:25:49 +0000

    Styled-components added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,17 @@
 import React from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { theme } from '../lib/styles'
 
 export const App = () => (
-    <div>
-    Hello World!
-    </div>
+    <ThemeProvider theme={theme}>
+        <AppContainer>
+            Hello World!!!
+        </AppContainer>
+    </ThemeProvider>
 )
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: ${({theme}) => theme.colors.background}
+`
---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,12 @@
+import { AppTheme } from "../types";
+
+export const theme : AppTheme = {
+    colors: {
+        primary: '#A6AEBF',
+        background: '#D0E8C5',
+        foreground: '#C5D3E8',
+        error: '#AF1740',
+        typography: '#FFF8DE',
+        input: '#133E87',
+    }
+}
